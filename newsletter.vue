c07da6ddd89666d147924c732aa514afecbe5b31
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2018-06-04 15:48:42 -0400

    NEW Vestar Template 1.0  /newsletter.vue

--- newsletter.vue
+++ newsletter.vue
@@ -71,6 +71,7 @@
             mounted () {
                 console.log(this.$route.query)
                 this.form_data.name = this.$route.query.name;
+                $("#newsletter_name").val(this.form_data.name);
                 this.form_data.email = this.$route.query.email;
                 $("#newsletter_email").val(this.form_data.email);
             },
