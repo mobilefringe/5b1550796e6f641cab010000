4efb6e1be72c0485b20941bc591bdf35a34596c0
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2018-06-05 11:35:04 -0400

    NEW Vestar Template 1.0  /events.vue

--- events.vue
+++ events.vue
@@ -17,7 +17,7 @@
                     <div class="row">
                         <div v-if="eventList" v-for="event in eventList">
                             <div class="col-md-12">
-                                <p>{{ event.end_date | moment("MMMM YYYY", timezone)}}</span></p>    
+                                <p>{{ event.end_date | moment("MMMM YYYY", timezone)}}</p>    
                             </div>
                             <div class="col-md-4">
                                 <img :src="event.image_url" :alt="'Event: ' + event.name" class="max_img" />   
