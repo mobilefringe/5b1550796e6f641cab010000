eb065d426f2292b8bb3b29e02e18c56ecd68c1fa
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2018-06-06 10:11:15 -0400

    NEW Vestar Template 1.0  /stores.vue

--- stores.vue
+++ stores.vue
@@ -1,38 +1,53 @@
 <template>
     <div> <!-- without an outer container div this component template will not render -->
-        <loading-spinner v-if="!dataLoaded"></loading-spinner>
+        <loader v-if="!dataLoaded"></loader>
         <transition name="fade">
             <div v-if="dataLoaded" v-cloak>
-                <div class="inside_page_header">
-                    <div class="main_container position_relative">
-                        <h2>Directory</h2>
-                    </div>
-                </div>
-                <div class="main_container margin_30">
-                    <div class="hidden_phone">
-                        <!--<div class="row store_nav">-->
-                        <!--    <div class="col-md-3">-->
-                        <!--        <a class="store_nav_link active_store_nav" href="/stores">Directory</a>-->
-                        <!--    </div>-->
-                        <!--    <div class="col-md-3">-->
-                        <!--        <a class="store_nav_link" href="/map">Centre Map</a>-->
-                        <!--    </div>-->
-                        <!--    <div class="col-md-3">-->
-                        <!--        <span>Sort By: </span>-->
-                        <!--        <a class="store_nav_link" v-on:click="changeMode('alphabetical')">Alphabetical</a>-->
-                        <!--    </div>-->
-                        <!--    <div class="col-md-3">-->
-                        <!--        <v-select v-model="selectedCat" :options="dropDownCats" :searchable="false" :on-change="filteredByCategory" class="category-select" :placeholder="$t('stores_page.sort_by_cats')" id="selectByCat"></v-select>-->
-                        <!--    </div>-->
-                        <!--</div>-->
-                        <!--<div class="row">-->
-                        <!--    <div class="col-md-12">-->
-                        <!--        <span class="pull-right"><span class="promo_exist"><i class="fas fa-tag"></i></span> Promotion</span>  -->
-                        <!--        <span class="pull-right"><span class="new_store"><i class="fas fa-star"></i></span> New Store </span>-->
-                        <!--        <span class="pull-right"><span class="coming_soon_store"><i class="far fa-clock"></i></span> Coming Soon Store</span>-->
-                        <!--    </div>-->
-                        <!--</div>-->
-                        <!-- Logo View -->
+        		<div class="page_header" v-if="storeBanner" v-bind:style="{ backgroundImage: 'url(' + storeBanner.image_url + ')' }">
+        			<div class="site_container">
+        				<div class="header_content caps">
+        				    <p>{{ $t("stores_page.header_desc") }} {{ property.name }}</p>
+        					<h1>{{ $t("stores_page.directory") }}</h1>
+        				</div>
+        			</div>
+        		</div>
+        		<div class="site_container page_content">
+        			<div class="stores_header_container">
+        			    <div class="stores_header_line hidden_phone"></div>
+        				<div class="stores_button_container search position_relative">
+        					<search-component :list="allStores" :placeholder="$t('stores_page.find_your_store')" suggestion-attribute="name" v-model="search_result" @select="onOptionSelect" class="text-left">
+        						<template slot="item" scope="option" class="manual">
+        							<article class="media">
+        								<p>{{ option.data.name }}</p>
+        							</article>
+        						</template>
+        					</search-component>
+        					<img src="//codecloud.cdn.speedyrails.net/sites/5ac3cb4b6e6f640eec120000/image/png/1517497861636/search_icon_2x.png" id="search_icon" alt="">
+        				</div>
+        				<div class="stores_header_line hidden_phone"></div>
+        				<div class="stores_button_container category">
+        					<v-select 
+        					    v-model="selectedCat" 
+        					    :options="dropDownCats" 
+        					    :searchable="false" 
+        					    :on-change="filterByCategory" 
+        					    class="category-select" 
+        					    :placeholder="$t('stores_page.sort_by_cats')" 
+        					    id="selectByCat"
+        				    ></v-select>
+        				</div>
+        				<div class="stores_header_line hidden_phone"></div>
+        				<div class="stores_button_container map_link hidden_phone">
+        				    <router-link to="/map">
+        				        <div>{{$t("stores_page.view_map")}}</div>
+        				    </router-link>
+        				</div>
+        				<div class="stores_header_line hidden_phone"></div>
+        			</div>
+        			<div class="margin_40">
+    			        <button class="submit_btn animated_btn swing_in" @click="toggleView()">{{ toggleText }}</button>
+        			</div>
+        			<!-- Logo View -->
         			<div v-if="logoView">
             			<div v-masonry transition-duration="0.3s" item-selector=".stores-grid-item">
                             <transition-group name="custom-classes-transition" enter-active-class="animated fadeIn" leave-active-class="animated fadeOut" tag="div">
@@ -73,145 +88,231 @@
                             </div>
                         </transition-group>
                     </div>
-                    </div>
-                    <div class="visible_phone">
-                        <div class="row margin_40">
-                            <div class="col-md-12 mobile_store_select">
-                                <v-select 
-                                    :options="storesList" 
-                                    :placeholder="'Select A Store'" 
-                                    :searchable="false" 
-                                    :label="'name'" 
-                                    :on-change="dropPin"
-                                ></v-select> 
-                            </div>
-                        </div>
-                        <div class="row">
-                            <div class="col-md-12">
-                                <mapplic-png-map ref="pngmap_ref" :height="700" :hovertip="true" :storelist="storesList" :floorlist="floorList" :bindLocationOpened="true" :svgWidth="property.map_image_width" :svgHeight="property.map_image_height" :showPin="true" tooltiplabel="View Store Details"></mapplic-png-map>
-                            </div>
-                        </div>    
-                    </div>
-                </div>
-            </div>
-        </transition>
-    </div>
+        		</div>
+	        </div>
+	    </transition>
+	</div>
 </template>
-
+<style>
+    .store_logo_container {
+        margin: 0;
+        border: none !important;
+    }
+    .store_details {
+        position: absolute;
+        top: 0;
+        right: 0;
+        bottom: 0;
+        left: 0;
+        width: 100%;
+        height: 100%;
+        display: none;
+        background-color: rgba(255, 255, 255, 0.4);
+        transition: all 0.3s ease;
+    }
+    .store_logo_container:hover .store_img {
+        display: block;
+        -webkit-filter: blur(3px);
+        -moz-filter: all .25s ease;
+        -o-filter: all .25s ease;
+        -ms-filter: all .25s ease;
+        filter: blur(3px);
+    }
+    .store_logo_container:hover .store_tag {
+        display: none;
+    }
+    .store_logo_container:hover .store_details {
+        display: block;
+    }
+    .store_logo_container:hover .store_details .store_text {
+        position: absolute;
+        top: 50%;
+        left: 50%;
+        transform: translate(-50%, -50%);
+        font-size: 0.75rem;
+        color: #fff;
+        line-height: 22px;
+        letter-spacing: 0.6px;
+        text-transform: uppercase;
+        text-align: center;
+    }
+    .listView {
+        column-count: 2;
+    }
+</style>
 <script>
-    define(["Vue", "vuex", "vue-select", "vue!mapplic-png-map"], function(Vue, Vuex, VueSelect, MapplicComponent) {
-        Vue.component('v-select', VueSelect.VueSelect);
-        return Vue.component("stores-component", {
+    define(["Vue", "vuex", "vue-select", "jquery", "smooth-zoom", "vue!png-map", "vue!search-component", "masonry" , "vue-masonry-plugin"], function(Vue, Vuex, VueSelect, $, smoothZoom, PNGMapComponent, SearchComponent, masonry, VueMasonryPlugin) {
+        Vue.use(VueMasonryPlugin.default);
+        return Vue.component("stores-m-component", {
             template: template, // the variable template will be injected
             data: function() {
                 return {
                     dataLoaded: false,
-                    sortByStores: true,
-                    listOne: null,
-                    listTwo: null,
+                    storeBanner : null,
+                    windowWidth: 0,
+                    selectedCat: null,
                     filteredStores: null,
-                    selectedCat: "Select a Category"
+                    search_result : null,
+                    query: "",
+                    toggleText: "List View",
+                    logoView: true,
+                    listView: false
                 }
             },
-            created(){
+            created (){
                 this.loadData().then(response => {
-                    this.allStores;
-                    this.sortByStores = true;
+                    var temp_repo = this.findRepoByName('Directory Banner').images;
+                    if(temp_repo != null) {
+                        this.storeBanner = temp_repo[0];
+                    } else {
+                        this.storeBanner = "http://via.placeholder.com/1920x400/4f6726/4f6726";
+                    }
+                    
                     this.dataLoaded = true;
+
+                    // console.log(this.$route)
+                    // this.query = this.$route.query.category
+                    // if(this.query == "fast_food"){
+                    //   this.selectedCat = "Fast Food";
+                    //   this.filterByCategory();
+                    // } else {
+                    //     this.selectedCat = "All";
+                    //     this.filteredStores = this.allStores;
+                    // }
+                });
+            },
+            watch: {
+                windowWidth: function() {
+                    if (this.windowWidth <= 768) {
+                        this.mobile_store = true;
+                    } else {
+                        this.mobile_store = false;
+                    }
+                }
+            },
+            mounted() {
+                this.$nextTick(function() {
+                    window.addEventListener('resize', this.getWindowWidth);
+                    //Init
+                    this.getWindowWidth();
                 });
             },
             computed: {
                 ...Vuex.mapGetters([
                     'property',
+                    'timezone',
                     'processedStores',
                     'processedCategories',
                     'storesByAlphaIndex',
                     'storesByCategoryName',
+                    'findCategoryById',
                     'findCategoryByName',
-                    'findCategoryById'
-                ]), 
+                    'findRepoByName'
+                ]),
                 allStores() {
-                    var listOne = [];
-                    var listTwo = [];
-                    _.forEach( this.processedStores , function( value, key ) {
-                        var starter = "A";
-                        var breaker = "M";
-                        var store_initial = _.toUpper(value.name[0]);
-                        if (store_initial.charCodeAt(0) <= breaker.charCodeAt(0) && store_initial.charCodeAt(0) >= starter.charCodeAt(0)){
-                            listOne.push(value);
-                        } else {
-                            listTwo.push(value);    
-                        }
+                    var store_list = this.processedStores
+                    var vm = this;
+                    var hover_image = "";
+                    _.forEach(store_list, function(value, key) {
+                        if(value.assets != undefined){
+                            //Stores JSON
+                            var store_id = value.id
+                            vm.$store.dispatch('LOAD_PAGE_DATA', { url: vm.property.mm_host + "/api/v4/thegateway/stores/" + store_id + "/store_files.json" }).then(response => {
+                                if(response.data != undefined) {
+                                    hover_image = response.data.store_files[0].url
+                                    value.hover_img = 'https://www.mallmaverick.com' + hover_image
+                                    // vm.filteredStores = null;
+                                    // vm.filteredStores = store_list; 
+                                    // vm.filteredStores[key].hover_img = 'https://www.mallmaverick.com' + hover_image
+                                }
+                            }, error => {
+                                console.error("Could not retrieve data from server. Please check internet connection and try again.");
+                                vm.$router.replace({ name: 'home' });
+                            });
+                        }    
                     });
-                    this.listOne = _.groupBy(listOne, store => (isNaN(_.upperCase(store.name.charAt(0))) ? _.upperCase(store.name.charAt(0)) : "#"));
-                    this.listTwo = _.groupBy(listTwo, store => (isNaN(_.upperCase(store.name.charAt(0))) ? _.upperCase(store.name.charAt(0)) : "#"));
+                    this.filteredStores = store_list;
+                    return store_list
+                },
+                allCatergories() {
+                    return this.processedCategories;
                 },
                 dropDownCats() {
                     var cats = _.map(this.processedCategories, 'name');
                     cats.unshift('All');
                     return cats;
                 },
-                storesList() {
-                    this.processedStores.map(function(store){
-                        store.zoom = 1;
-                    })
-                    return this.processedStores;
-                },
                 getPNGurl() {
                     return "https://www.mallmaverick.com" + this.property.map_url;
                 },
-                pngMapRef() {
-                    return this.$refs.pngmap_ref;
+                svgMapRef() {
+                    return _.filter(this.$children, function(o) {
+                        return (o.$el.className == "svg-map")
+                    })[0];
                 },
-                floorList () {
-                    var floor_list = [];
-                    
-                    var floor_1 = {};
-                    floor_1.id = "first-floor";
-                    floor_1.title = "Floor 1";
-                    floor_1.map = this.getPNGurl;
-                    floor_1.z_index = 1;
-                    floor_1.show = true;
-                    
-                    floor_list.push(floor_1);
-                    return floor_list;
+                filterByCategory() {
+                    category_id = this.selectedCat;
+                    if (category_id == "All" || category_id == null || category_id == undefined) {
+                        category_id = "All";
+                    } else {
+                        category_id = this.findCategoryByName(category_id).id;
+                    }
+
+                    if (category_id == "All") {
+                        this.filteredStores = this.allStores;
+                    } else {
+                        var find = this.findCategoryById;
+                        var filtered = _.filter(this.allStores, function(o) {
+                            return _.indexOf(o.categories, _.toNumber(category_id)) > -1;
+                        });
+                        this.filteredStores = filtered;
+                    }
+                    var el = document.getElementById("selectByCat");
+                    if(el) {
+                        el.classList.remove("open");
+                    }
                 }
             },
             methods: {
-                loadData: async function () {
+                loadData: async function() {
                     try {
-                        let results = await Promise.all([this.$store.dispatch("getData", "categories")]);
-                        return results;
+                        // avoid making LOAD_META_DATA call for now as it will cause the entire Promise.all to fail since no meta data is set up.
+                        let results = await Promise.all([this.$store.dispatch("getData", "categories"), this.$store.dispatch("getData", "repos")]);
+                        
                     } catch (e) {
                         console.log("Error loading data: " + e.message);
                     }
                 },
-                changeMode (mode) {
-                    this.sortByStores = true;
+                changeMode(mode) {
+                    this.listMode = mode;
                 },
-                filteredByCategory (cat_id) {
-                    if(cat_id == "Select a Category" || cat_id == "All" || cat_id == null || cat_id == undefined){
-                        category_id = "All";
-                    } else {
-                        category_id = this.findCategoryByName(cat_id).id;
-                    }
-                    
-                    if(category_id == "All"){
-                        this.sortByStores = true;
-                    } else {
-                        this.sortByStores = false;
-                        var find = this.findCategoryById;
-                        var filtered = _.filter(this.processedStores, function(o) {return _.indexOf(o.categories, _.toNumber(category_id)) > -1; });
-                        _.forEach(filtered, function(value, i) {
-                            value.currentCategory = find(category_id).name;
-                        });
-                        sortedCats = _.groupBy(filtered, store => store.currentCategory);
-                        this.filteredStores = sortedCats;
-                    }
+                toggleView() {
+                    if (this.logoView) {
+                        this.toggleText = "Logo View"
+                        this.listView = true;
+                        this.logoView = false;
+                    } else if (this.listView) {
+                        this.toggleText = "List View"
+                        this.logoView = true;
+                        this.listView = false;
+                    } 
                 },
-                dropPin(store) {
-                    this.pngMapRef.showLocation(store.id);
+                updateSVGMap(map) {
+                    this.map = map;
+                },
+                addLandmark(store) {
+                    this.svgMapRef.addMarker(store);
+                },
+                getWindowWidth(event) {
+                    this.windowWidth = window.innerWidth;
+                },
+                onOptionSelect(option) {
+                    this.search_result = "";
+                    this.$router.push("/stores/" + option.slug);
                 }
+            },
+            beforeDestroy: function() {
+                window.removeEventListener('resize', this.getWindowWidth);
             }
         });
     });
