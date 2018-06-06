0a203551474c6010ec79c906aa814cae85d6fd50
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2018-06-06 15:17:53 -0400

    NEW Vestar Template 1.0  /stores.vue

--- stores.vue
+++ stores.vue
@@ -76,9 +76,6 @@
                     			    <router-link :to="'/stores/'+ store.slug">
                     			        <p>{{ store.name }}</p>
                     			    </router-link>
-                    			    <span v-if="store.total_published_promos"><i class="fa fa-tag"></i></span>
-                                    <span v-if="store.is_new_store"><i class="fa fa-star"></i></span>
-                                    <span v-if="store.is_coming_soon_store"><i class="fa fa-clock"></i></span>
                     			</div>
                             </div>
                         </transition-group>
