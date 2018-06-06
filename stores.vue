8d16962ae04796088a58ea0fe89e3327c59e563b
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2018-06-06 10:44:24 -0400

    NEW Vestar Template 1.0  /stores.vue

--- stores.vue
+++ stores.vue
@@ -44,13 +44,13 @@
                             	    <div class="store_logo_container">
                             	        <router-link :to="'/stores/'+ store.slug">
                                 			<img class="store_img" :src="store.image_url" alt="">
-                                			<div class="store_tag store_promotion" v-if="store.total_published_promos">-->
+                                			<div class="store_tag" v-if="store.total_published_promos">-->
             									<div class="store_tag_text">Promotion</div>
             								</div>
-            								<div class="store_tag coming_soon" v-if="!store.total_published_promos && !store.is_new_store && store.is_coming_soon_store">
+            								<div class="store_tag" v-if="!store.total_published_promos && !store.is_new_store && store.is_coming_soon_store">
             									<div class="store_tag_text">Coming Soon</div>
             								</div>
-            								<div class="store_tag new_store" v-if="!store.total_published_promos && !store.is_coming_soon_store && store.is_new_store">
+            								<div class="store_tag" v-if="!store.total_published_promos && !store.is_coming_soon_store && store.is_new_store">
             									<div class="store_tag_text">New Store</div>
             								</div>
             								<div class="store_details">
