37e6a3c224d8d3ac52ab6a92ec28994c5b11689c
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2018-06-07 11:26:44 -0400

    NEW Vestar Template 1.0  /store_details.vue

--- store_details.vue
+++ store_details.vue
@@ -206,10 +206,10 @@
                 dropPin(store) {
                     this.$refs.pngmap_ref.showLocation(store.id);
                 },
-                isMultiDay(promo) {
+                isMultiDay(item) {
                     var timezone = this.timezone
-                    var start_date = moment(promo.start_date).tz(timezone).format("MM-DD-YYYY")
-                    var end_date = moment(promo.end_date).tz(timezone).format("MM-DD-YYYY")
+                    var start_date = moment(item.start_date).tz(timezone).format("MM-DD-YYYY")
+                    var end_date = moment(item.end_date).tz(timezone).format("MM-DD-YYYY")
                     if (start_date === end_date) {
                         return false
                     } else {
