05d6cc2aed274a94781d4a0baad9f085f60797c3
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2018-06-07 10:10:12 -0400

    NEW Vestar Template 1.0  /store_details.vue

--- store_details.vue
+++ store_details.vue
@@ -51,6 +51,14 @@
                                     <div class="col-md-6" v-if="storePromotions" v-for="item in storePromotions">
                                         <img :src="item.image_url" :alt="item.name" class="img_max"/>
                                         
+                                        <figure class="snip0016">
+	                                        <img :src="item.image_url" :alt="item.name"/>
+                                        	<figcaption>
+                                        		<h2>I think <span>nighttime</span> is dark</h2>
+                                        		<p>So you can imagine your fears with less distraction.</p>
+                                        		<a href="#"></a>
+                                        	</figcaption>			
+                                        </figure>
                                     </div>
                                 </div>
                             </div>
