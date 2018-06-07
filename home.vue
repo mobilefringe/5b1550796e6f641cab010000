e669b0499d7613b3a1184fb513e6b18cb9c4308f
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2018-06-07 15:18:39 -0400

    NEW Vestar Template 1.0  /home.vue

--- home.vue
+++ home.vue
@@ -28,8 +28,8 @@
                             <div class="insta-feed-container">
                                 <div class="insta-feed-image " v-for="(item, index) in instaFeed">
                                     <a :href="item.link" target="_blank">
-                                        <!--<img :src="item.images.standard_resolution.url" :alt="item.id"/>-->
-                                        <img :src="item.images.thumbnail.url" :alt="item.id"/>
+                                        <img :src="item.images.standard_resolution.url" :alt="item.id"/>
+                                        <!--<img :src="item.images.thumbnail.url" :alt="item.id"/>-->
                                         <div class="insta_content">
                                             <p class="insta_caption">{{ item.caption.text }}</p>
                                             <p class="insta_user">@{{ item.user.username }}</p>
