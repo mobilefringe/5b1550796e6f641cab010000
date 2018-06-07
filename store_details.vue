735d9961c406dc3c95415b84a62c41a4bc3941c4
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2018-06-07 12:56:59 -0400

    NEW Vestar Template 1.0  /store_details.vue

--- store_details.vue
+++ store_details.vue
@@ -154,7 +154,7 @@
                     this.updateCurrentStore(this.$route.params.id);
                 },
                 currentStore: function () {
-                    console.log(currentStore)
+                    console.log(this.currentStore)
                     if (_.includes(this.currentStore.store_front_url_abs, 'missing')) {
                         this.currentStore.store_front_url_abs = "http://placehold.it/600x600"
                     }
