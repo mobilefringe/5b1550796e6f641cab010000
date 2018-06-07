ede84d7b31f3a2bbbbdd22957053a9f335328410
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2018-06-07 13:22:12 -0400

    NEW Vestar Template 1.0  /store_details.vue

--- store_details.vue
+++ store_details.vue
@@ -203,10 +203,10 @@
                     var temp_coupon = [];
                     _.forEach(this.currentStore.coupons, function(value, key) {
                         var current_coupon = vm.findCouponById(value);
-                        
-                        if (_.includes(current_coupon.image_url, 'missing')) {
-                            current_coupon.image_url = "http://placehold.it/1560x800/757575";
-                        }
+                        console.log(current.coupon)
+                        // if (_.includes(current_coupon.image_url, 'missing')) {
+                        //     current_coupon.image_url = "http://placehold.it/1560x800/757575";
+                        // }
 
                         temp_coupon.push(current_coupon);
                     }); 
