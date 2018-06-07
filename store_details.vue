e778fffb4c5ed7d27af2228a882729ad8f796c86
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2018-06-07 13:24:48 -0400

    NEW Vestar Template 1.0  /store_details.vue

--- store_details.vue
+++ store_details.vue
@@ -252,6 +252,7 @@
                 loadData: async function () {
                     try {
                         let results = await Promise.all([this.$store.dispatch("getData", "stores"), this.$store.dispatch("getData","events"), this.$store.dispatch("getData","promotions"), this.$store.dispatch("getData","coupons")]);
+                        console.log(results)
                     } catch (e) {
                         console.log("Error loading data: " + e.message);
                     }
