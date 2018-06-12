baadc882f831f0063d438a753916be436b14c2f2
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2018-06-12 14:07:21 -0400

    NEW Vestar Template 1.0  /contact.vue

--- contact.vue
+++ contact.vue
@@ -5,7 +5,7 @@
             <div v-if="dataLoaded" v-cloak>
                 <div class="inside_page_header">
                     <div class="main_container position_relative">
-                        <h2>Center Information</h2>
+                        <h2>Contact Us</h2>
                     </div>
                 </div>
                 <div class="main_container">
