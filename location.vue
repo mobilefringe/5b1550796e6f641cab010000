d1fce8c1d8f8f1d228ea1db6d89644fd404416b6
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2018-06-12 14:49:11 -0400

    NEW Vestar Template 1.0  /location.vue

--- location.vue
+++ location.vue
@@ -20,7 +20,8 @@
                         </div>
                     </div>
                 </div>
-                <iframe title="Map" width="100%" height="400px" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"  :src="'http://maps.google.nl/maps?q=' + getPropertyAddress + '&amp;hl=en&amp;ie=UTF8&amp;t=v&amp;hnear=' + getPropertyAddress + '&amp;z=16&amp;output=embed'"></iframe>
+                <google-map :property="property" :zoom="18"></google-map>
+                <!--<iframe title="Map" width="100%" height="400px" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"  :src="'http://maps.google.nl/maps?q=' + getPropertyAddress + '&amp;hl=en&amp;ie=UTF8&amp;t=v&amp;hnear=' + getPropertyAddress + '&amp;z=16&amp;output=embed'"></iframe>-->
                 <div class="main_container">
                     <div class="row">
                         <div class="col-md-6">
