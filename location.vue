93bee1fa1617f751789b7158b581c6b9b6cc79fd
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2018-06-12 14:21:34 -0400

    NEW Vestar Template 1.0  /location.vue

--- location.vue
+++ location.vue
@@ -49,6 +49,7 @@
             },
             created() {
                 this.loadData().then(response => {
+                    console.log(response)
                     // this.currentPage = response[0].data;
                     this.dataLoaded = true;
                 });
