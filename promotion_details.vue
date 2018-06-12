d5161f574592307224caa9cf4418dd0d68935a74
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2018-06-12 13:01:19 -0400

    NEW Vestar Template 1.0  /promotion_details.vue

--- promotion_details.vue
+++ promotion_details.vue
@@ -59,6 +59,7 @@
         margin-bottom: 60px;
     }
 </style>
+
 <script>
     define(["Vue", "vuex", "moment", "moment-timezone", "vue-moment", "vue-lazy-load",  "vue-social-sharing", "json!site.json"], function(Vue, Vuex, moment, tz, VueMoment, VueLazyload, SocialSharing, site) {
         Vue.use(VueLazyload);
