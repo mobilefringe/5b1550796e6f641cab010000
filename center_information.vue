2e663d74c923d5e30926ff8280f3d32ca313ef9d
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2018-06-12 13:46:08 -0400

    NEW Vestar Template 1.0  /center_information.vue

--- center_information.vue
+++ center_information.vue
@@ -5,7 +5,7 @@
             <div v-if="dataLoaded" v-cloak>
                 <div class="inside_page_header">
                     <div class="main_container position_relative">
-                        <h2>Newsletter</h2>
+                        <h2>Center Information</h2>
                     </div>
                 </div>
                 <div class="main_container">
