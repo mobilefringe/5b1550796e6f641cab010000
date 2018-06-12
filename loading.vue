05ea8ec6f512eebf7c5894c83b2414879068f36f
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2018-06-12 16:38:47 -0400

    NEW Vestar Template 1.0  /loading.vue

--- loading.vue
+++ loading.vue
@@ -1,7 +1,8 @@
 <template>
     <div v-if="dataLoaded" id="overlay">
         <div class="loading-container">
-            <vue-simple-spinner size="huge" :message="property.name + ' is Loading...'"></vue-simple-spinner>
+            <div class="loader"></div>
+            <p>{{ property.name }} is Loading...</p>
         </div>
     </div>
 </template>
@@ -41,65 +42,7 @@
 </script>
 
 <style>
-    .loader,
-.loader:before,
-.loader:after {
-  background: #ffffff;
-  -webkit-animation: load1 1s infinite ease-in-out;
-  animation: load1 1s infinite ease-in-out;
-  width: 1em;
-  height: 4em;
-}
-.loader {
-  color: #ffffff;
-  text-indent: -9999em;
-  margin: 88px auto;
-  position: relative;
-  font-size: 11px;
-  -webkit-transform: translateZ(0);
-  -ms-transform: translateZ(0);
-  transform: translateZ(0);
-  -webkit-animation-delay: -0.16s;
-  animation-delay: -0.16s;
-}
-.loader:before,
-.loader:after {
-  position: absolute;
-  top: 0;
-  content: '';
-}
-.loader:before {
-  left: -1.5em;
-  -webkit-animation-delay: -0.32s;
-  animation-delay: -0.32s;
-}
-.loader:after {
-  left: 1.5em;
-}
-@-webkit-keyframes load1 {
-  0%,
-  80%,
-  100% {
-    box-shadow: 0 0;
-    height: 4em;
-  }
-  40% {
-    box-shadow: 0 -2em;
-    height: 5em;
-  }
-}
-@keyframes load1 {
-  0%,
-  80%,
-  100% {
-    box-shadow: 0 0;
-    height: 4em;
-  }
-  40% {
-    box-shadow: 0 -2em;
-    height: 5em;
-  }
-}
+    
 
     #overlay {
         position: fixed;
