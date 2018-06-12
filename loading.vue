65647e43bb97b23d5dcfd01b9c9d4465589230c3
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2018-06-12 16:39:29 -0400

    NEW Vestar Template 1.0  /loading.vue

--- loading.vue
+++ loading.vue
@@ -40,32 +40,3 @@
         });
     });
 </script>
-
-<style>
-    
-
-    #overlay {
-        position: fixed;
-        /* Sit on top of the page content */
-        width: 100%;
-        /* Full width (cover the whole page) */
-        height: 100%;
-        /* Full height (cover the whole page) */
-        overflow: hidden;
-        top: 0;
-        left: 0;
-        right: 0;
-        bottom: 0;
-        background-color: #ffffff;
-        z-index: 999;
-        display: -webkit-box;
-        display: -webkit-flex;
-        display: -moz-box;
-        display: -moz-flex;
-        display: -ms-flexbox;
-        display: flex;
-    }
-    .loading-container .vue-simple-spinner-text{
-        line-height: 1;
-    }
-</style>
