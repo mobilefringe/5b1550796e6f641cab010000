ce1255005b225a1678ac3b61371ad25cd6ddacf1
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2018-06-15 11:53:07 -0400

    NEW Vestar Template 1.0  /center_information.vue

--- center_information.vue
+++ center_information.vue
@@ -64,15 +64,14 @@
             created() {
                 this.loadData().then(response => {
                     var temp_repo = this.findRepoByName('Center Information Images');
-                    console.log(temp_repo)
                     if(temp_repo) {
                         var three_imgs = _.slice(temp_repo.images, [0], [3])
                         this.pageImages = three_imgs;
-                        console.log(this.pageImages)
+
                         var one_img = temp_repo.images[3];
                         this.lowerBanner = one_img;
-                        console.log(this.lowerBanner)
                     }
+                    
                     this.main = response[1].data;
                     this.amenities = response[1].data.subpages
                     this.dataLoaded = true;
