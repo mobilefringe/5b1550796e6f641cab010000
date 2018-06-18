1d97eb1358dc56c0ada98a53c960e15a28e59a09
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2018-06-18 10:15:10 -0400

    NEW Vestar Template 1.0  /coupons.vue

--- coupons.vue
+++ coupons.vue
@@ -139,15 +139,16 @@
                     this.$nextTick(function() {
                         var current_coupon_id = item.id;
                         if (item.is_in_cart === false) {
-                            item.is_in_cart = true;
+                            this.$nextTick(function() {
+                                item.is_in_cart = true;
+                            });
                             $(".basket_number").text(parseInt($(".basket_number").text())+1);
         
                             this.selected_coupon_id.push(current_coupon_id);
                         } else {
-                            item.is_in_cart = false;
-                            // $(el).attr("is_in_cart", "false");
-                            // $(el).attr("src", "//codecloud.cdn.speedyrails.net/sites/59d7b1c56e6f64669e8d0000/image/png/1508346683000/Basket@2x.png");
-                            // $(el).parent().parent().removeClass("green_selected_box");
+                            this.$nextTick(function() {
+                                item.is_in_cart = false;
+                            });
                             $(".basket_number").text(parseInt($(".basket_number").text())-1);
                             //remove coupon from list variable
                             this.selected_coupon_id = $.grep(this.selected_coupon_id, function(val, i) {
