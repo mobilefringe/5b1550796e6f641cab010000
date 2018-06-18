ea2be5faeca319da2658334896bb8148a6259fe8
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2018-06-18 12:37:14 -0400

    NEW Vestar Template 1.0  /coupon_basket.vue

--- coupon_basket.vue
+++ coupon_basket.vue
@@ -31,7 +31,7 @@
         		        </div>
         		    </div>
                     <div class="row">
-                        <div v-for="(item, index) in couponList" v-on:remove="couponList.splice(index, 1)" class="col-md-6 col-sm-6 col-xs-12">
+                        <div v-for="(item, index) in couponList" :key="item.id" v-on:remove="couponList.splice(index, 1)" class="col-md-6 col-sm-6 col-xs-12">
                             <div :id="item.id" class="row coupon_container">
                                 <div class="col-md-6 col-sm-6 col-xs-12">
                                     <div class="coupon_img">
