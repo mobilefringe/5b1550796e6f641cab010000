26dbbd9e8b12b2d5caa72ae9e4504b7ce30896b6
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2018-06-19 13:54:01 -0400

    NEW Vestar Template 1.0  /page_details.vue

--- page_details.vue
+++ page_details.vue
@@ -54,7 +54,6 @@
                     var _this = this;
                     this.$store.dispatch('LOAD_PAGE_DATA', { url: this.property.mm_host + "/pages/" + this.id + ".json" }).then(function (response) {
                         _this.currentPage = response.data;
-                        console.log(_this.$breadcrumbs)
                         _this.$breadcrumbs[0].meta.breadcrumb = _this.currentPage.title
                         _this.dataLoaded = true;
                     }, function (error) {
