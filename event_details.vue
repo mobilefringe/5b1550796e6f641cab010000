cbb552e438fe3bc74f879dd2a72e9343f12321ac
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2018-06-20 18:21:48 -0400

    NEW Vestar Template 1.0  /event_details.vue

--- event_details.vue
+++ event_details.vue
@@ -9,7 +9,7 @@
                     </div>
                 </div>
                 <div class="main_container">
-                    <div class="row">
+                    <div class="row hidden-xs">
                         <div class="col-md-12">
                             <breadcrumb></breadcrumb>
                         </div>
