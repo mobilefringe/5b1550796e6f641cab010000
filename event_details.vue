cd1a5f914fe6dd191cbfa9bfdbe9583b5c4a9102
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2018-06-20 10:27:38 -0400

    NEW Vestar Template 1.0  /event_details.vue

--- event_details.vue
+++ event_details.vue
@@ -34,7 +34,7 @@
                             <div class="col-md-12">
                                 <div class="row margin_30">
                                     <div class="col-md-12">
-                                        <router-link to="/promotions">
+                                        <router-link to="/events">
                     		                <div class="animated_btn pull-left">Back to Events</div>    
                     		            </router-link>    
                                     </div>
