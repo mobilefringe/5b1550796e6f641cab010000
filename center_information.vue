c6bf8f7e7d4317cafe03697dd229d10ac3ee5065
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2018-06-20 13:31:13 -0400

    NEW Vestar Template 1.0  /center_information.vue

--- center_information.vue
+++ center_information.vue
@@ -88,6 +88,7 @@
             },
             methods: {
                 loadData: async function () {
+                    this.property.mm_host = this.property.mm_host.replace("http:", "");
                     try {
                         let results = await Promise.all([this.$store.dispatch("getData", "repos"), this.$store.dispatch('LOAD_PAGE_DATA', {url: this.property.mm_host + "/pages/cerritos-center-information.json"})]);
                         return results;
