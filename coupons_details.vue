26570219c26f447bd48197ad8eacdda0f84d7777
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2018-06-21 11:46:44 -0400

    NEW Vestar Template 1.0  /coupons_details.vue

--- coupons_details.vue
+++ coupons_details.vue
@@ -18,7 +18,7 @@
                         <div class="row">
                             <div class="col-md-2">
                                 <!--<img v-lazy="currentCoupon.image_url" :alt="'Promotion: ' + currentCoupon.name" class="margin_20 img_max"/>    -->
-                                <img v-lazy="https://placehold.it/500"  class="margin_20 img_max"/>    
+                                <img src="https://placehold.it/500"  class="margin_20 img_max"/>    
                             </div>
                             <div class="col-md-10">
                                 <!--<p v-if="currentCoupon.promotionable_type == 'Property'" class="event_store_name">{{ property.name }}</p>-->
@@ -30,7 +30,6 @@
                                 </p>
                                 <div class="event_desc event_details" v-html="currentCoupon.rich_description"></div>
                             </div>
-                            
                         </div>
                         <div class="row">
                             <div class="col-md-12">
