8ba20fe912d058edd0669ddddb90da7a8b66e779
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2018-06-28 15:36:23 -0400

    NEW Vestar Template 1.0  /contest.vue

--- contest.vue
+++ contest.vue
@@ -29,19 +29,19 @@
                             <form id="contest_form" class="form-horizontal padding_top_20" action="form-submit" v-on:submit.prevent="validateBeforeSubmit">
         						<div class="form-group ">
         							<div class="col-sm-6 col-xs-12" :class="{'has-error': errors.has('first_name')}">
-        								<label class="label" for="first_name">First Name</label>
+        								<label class="accessibility" for="first_name">First Name</label>
         								<input v-model="form_data.first_name" v-validate="'required:true'" class="form-control" :class="{'input': true}" name="first_name" type="text" placeholder="First Name" data-vv-delay="500" data-vv-as="first name">
         								<span v-show="errors.has('first_name')" class="form-control-feedback">{{ errors.first('first_name') }}</span>
         							</div>
         							<div class="col-sm-6 col-xs-12" :class="{'has-error': errors.has('last_name')}">
-        								<label class="label" for="last_name">Last Name</label>
+        								<label class="accessibility" for="last_name">Last Name</label>
         								<input v-model="form_data.last_name" v-validate="'required:true'" class="form-control" :class="{'input': true}" name="last_name" type="text" placeholder="Last Name" data-vv-delay="500" data-vv-as="last name">
         								<span v-show="errors.has('last_name')" class="form-control-feedback">{{ errors.first('last_name') }}</span>
         							</div>
         						</div>
         						<div class="form-group">
         							<div class="col-xs-12" :class="{'has-error': errors.has('email')}">
-        								<label class="label" for="email">Email</label>
+        								<label class="accessibility" for="email">Email</label>
         								<input v-model="form_data.email" v-validate="'required|email'" class="form-control" :class="{'input': true}" name="email" type="email" placeholder="Email" data-vv-delay="500" data-vv-as="email">
         								<span v-show="errors.has('email')" class="form-control-feedback">{{ errors.first('email') }}</span>
         							</div>
