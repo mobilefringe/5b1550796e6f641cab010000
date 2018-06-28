038a9b7936549e4f834bfb4ea691c1930e335c99
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2018-06-28 17:15:01 -0400

    NEW Vestar Template 1.0  /contest.vue

--- contest.vue
+++ contest.vue
@@ -73,6 +73,8 @@
         						    <div class="col-xs-12 margin_60">
         						        <p>For more details about personal privacy, please read our <a href="" target="_blank">Privacy Policy</a>.</p>
                                     </div>
+                                </div>
+                                <div class="form-group">
         							<div class="col-xs-12">
         								<button class="animated_btn" type="submit" :disabled="formSuccess">Submit</button>
         							</div>
