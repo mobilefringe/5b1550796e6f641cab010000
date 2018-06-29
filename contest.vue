145882646425498f83050c0aa182fcfb212b4e67
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-06-29 11:04:11 -0400

    NEW Vestar Template 1.0  /contest.vue

--- contest.vue
+++ contest.vue
@@ -166,6 +166,17 @@
                             });
                         }
                     })
+                },
+                serializeObject(obj) {
+                    console.log(obj);
+                    var newObj = [];
+                    _.forEach(obj, function(value, key) {
+                        var tempVal = {};
+                        tempVal.name = key;
+                        tempVal.value = value;
+                        newObj.push(tempVal);
+                    });
+                    return newObj;
                 }
             }
         });
