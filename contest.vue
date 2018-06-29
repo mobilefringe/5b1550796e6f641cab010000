5189dfebb8f0e2e2a66454312c7a31716323ecd1
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-06-29 15:46:26 -0400

    NEW Vestar Template 1.0  /contest.vue

--- contest.vue
+++ contest.vue
@@ -157,7 +157,7 @@
                                 data: contest_entry,
                                 success: function(data) {
                                     vm.formSuccess = true;
-                                    var container = vm.$el.getElementById("#send_contact_success");
+                                    var container = document,getElementById("#send_contact_success");
                                     container.scrollTop = container.clientHeight;
                                 },
                                 error: function(data){
